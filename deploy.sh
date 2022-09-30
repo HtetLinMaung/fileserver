42df9564732243893e6aa03483f5bab1ae538463
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-09-30 21:50:18 +0630

    update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git pull && sudo docker-compose down && sudo docker-compose up -d
+git pull && sudo docker image rm -f htetlinmaung/fileserver && sudo docker-compose down && sudo docker-compose up -d
