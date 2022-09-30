9c8d96d8bef4978dc01b10964072356b7dc61c69
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-10-01 06:11:38 +0630

    update

--- routes/fileserver/v1/files/:_id/index.js
+++ routes/fileserver/v1/files/:_id/index.js
@@ -1,25 +1,29 @@
-const { brewExpressFuncCreateOrFindAll } = require("code-alchemy");
+const {
+  brewExpressFuncFindOneOrUpdateOrDeleteByParam,
+} = require("code-alchemy");
 const path = require("path");
 const fs = require("fs");
-const File = require("../../../../models/File");
-const { handleAuthorization } = require("../../../../services/auth");
-const { storageFolderPath } = require("../../../../constants");
+const File = require("../../../../../models/File");
+const { handleAuthorization } = require("../../../../../services/auth");
+const { storageFolderPath } = require("../../../../../constants");
 
-module.exports = brewExpressFuncCreateOrFindAll(
+module.exports = brewExpressFuncFindOneOrUpdateOrDeleteByParam(
   File,
   {
     afterFunctionStart: handleAuthorization,
-    beforeCreate: (req) => {
+    beforeUpdate: (data, req) => {
       const userStorageFolder = path.join(
         storageFolderPath,
         req.body.createdby
       );
-
       if (!fs.existsSync(userStorageFolder)) {
         fs.mkdirSync(userStorageFolder);
       }
       const { file, name } = req.body;
       req.body.location = path.join(userStorageFolder, name);
+      if (req.body.location != data.location && fs.existsSync(data.location)) {
+        fs.rmSync(data.location);
+      }
       fs.writeFileSync(
         req.body.location,
         Buffer.from(
@@ -27,8 +31,16 @@ module.exports = brewExpressFuncCreateOrFindAll(
           "base64"
         )
       );
+
       delete req.body.file;
     },
+    beforeDelete: (data) => {
+      if (fs.existsSync(data.location)) {
+        fs.rmSync(data.location);
+      }
+    },
   },
+  "Data not found!",
+  "",
   "mongoose"
 );
